0ca53298bd37eb2184b79f5624fe18714d23c380
Eldon <alu0100777758@edu.ull.es>
2016-03-03 23:01:58 +0000

    completando oop

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,39 @@
 (function(exports) {
   "use strict";
+  var XRegExp = require('xregexp');
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
-    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
-    /* ademas de new Medida(45.2, "Km") */
+    var _valor;
+    var _tipo;
+    valortipo = XRegExp('(?<valor>  [0-9]+ ) -?  # valor  \n' +
+               '(?<tipo> [a-z]+ ) -?  # tipo  ', 'ix');
+    if(isNaN(valor)){
+    valmatch = XRegExp.exec(valor, valortipo);
+    _valor = valmatch.valor;
+    _tipo = valmatch.tipo;
+    }
+    return {
+      valor: function()           { return _valor; },
+      tipo:  function()           { return _tipo; }
+    };
   }
-  
+
   function Temperatura(valor,tipo)
   {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    Medida.call(this, numberOfCylinder, carType)
   }
-  
+
   function Celsius(valor)
   {
+    Temperatura.call(this,valor,'C')
   }
-  
+
   function Farenheit(valor)
   {
+    Temperatura.call(this, valor, 'F')
   }
-  
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -30,14 +44,14 @@
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
     valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,7 +61,7 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
